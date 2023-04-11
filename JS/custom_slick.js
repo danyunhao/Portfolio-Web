8cc2298cc5f818fcaa223e79e0c9d65d7cf22f02
danyunhao <danyunhao@gmail.com>
2023-04-11 14:35:40 +0800

    2023 Aorul 11

---
+++ JS/custom_slick.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+                
+//     $('.lazy').slick({
+//         lazyLoad: 'ondemand',
+//         slidesToShow: 3,
+//         slidesToScroll: 1
+//             });
+//     });
+
+
+
+    // $(function () {
+    //     $('•lazy').slick({
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1, 
+    //     arrows: true,
+    //     autoplay: true, autoplaySpeed: 2000,
+    //     dots: false,
+    //     });
+        
+    // });
